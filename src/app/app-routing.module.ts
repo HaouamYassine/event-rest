a535072455f9ef5cc3f4b01f77bd3ef24af9c37f
Akela <haouamyassine@gmail.com>
2023-02-21 10:30:22 +0100

    events-list-display

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CalendarComponent} from "./component/calendar/calendar.component";
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', redirectTo: 'events', pathMatch: 'full'},
+  {path: 'events', component: CalendarComponent}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
